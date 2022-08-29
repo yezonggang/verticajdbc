a225e3e25771ef576e1032ff156e7f625ae393ba
叶子叶 <717818895@qq.com>
2022-08-29 16:32:16 +0800

    一个表，对应一个sql，对应一个connection，对应要给session

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,10 @@ import utils.JDBCUtil;
 
 public class Main {
     public static void main(String[] args) {
-        // 针对表名做个循环:test1 表，test2表，见表语句在resources中的test.sql，52*65000的大表
-        //每个表开1个线程池
+        //针对表名进行循环:test1 表，test2表..........tesnN表，创建表的语句在resources中的test.sql，均是52*65000的大表
+        //每个表对应1个线程池，对应就是1个connection，在vertica上，就是session
         //每个线程池的大小和实际运行的线程数，可以指定，分别是threadPollNum和runningThredPollNum
-        //每个表又分别对应一个connection，在vertica上，就是session
+        //每个connection都是死循环，所以没有close
         for (int i=1;i<11;i++){
             new VerticJdbcPressTest(10,8,String.format("test%s",i),JDBCUtil.getConnection()).test();
         }
