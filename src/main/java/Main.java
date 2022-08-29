803a76adf4003114a8b986cf41391cc7105cda35
叶子叶 <717818895@qq.com>
2022-08-29 14:14:59 +0800

    优化成druid连接池

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,15 @@
-import jdbcDemos.JdbcRunnable;
 import jdbcDemos.VerticJdbcPressTest;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import utils.JDBCUtil;
-import utils.ThreadPollUtil;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
-import java.util.concurrent.ExecutorService;
+import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
         // 线程池大小，实际运行的线程池，表名
         Connection conn = JDBCUtil.getConnection();
+
         VerticJdbcPressTest verticJdbcPressTest001 = new VerticJdbcPressTest(20,10,"test2",conn);
         VerticJdbcPressTest verticJdbcPressTest002 = new VerticJdbcPressTest(20,10,"test3",conn);
         verticJdbcPressTest001.test();
--- src/main/java/utils/JDBCUtil.java
+++ src/main/java/utils/JDBCUtil.java
@@ -1,23 +1,44 @@
 package utils;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.pool.DruidDataSourceFactory;
 
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class JDBCUtil {
+
     public static Connection getConnection() {
+        ClassLoader classLoader = JDBCUtil.class.getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream("jdbc.properties");
+        Properties properties = new Properties();
+        try {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        DataSource datasouce  = null;
         Connection conn = null;
         try {
-            DruidDataSource dataSource = new DruidDataSource();
+/*            DruidDataSource dataSource = new DruidDataSource();
             dataSource.setDriverClassName("com.vertica.jdbc.Driver");
             dataSource.setUrl("jdbc:vertica://10.121.198.45:5433/dbadmin");
             dataSource.setUsername("dbadmin");
             dataSource.setPassword("passwd");
-            conn = dataSource.getConnection(1000);
+            conn = dataSource.getConnection(1000);*/
+            datasouce = DruidDataSourceFactory.createDataSource(properties);
+            conn = datasouce.getConnection();
         } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return conn;
     }
+
+
 }
